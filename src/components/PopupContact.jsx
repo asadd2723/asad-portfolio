aeb9f8f510a7a8cd1a028d817d81c07fbe98fd08
asadd2723 <asadu3040@gmail.com>
2024-07-11 18:05:45 +0500

    add more padding in popupcontact page

--- src/components/PopupContact.jsx
+++ src/components/PopupContact.jsx
@@ -7,7 +7,7 @@ function PopupContact({change}) {
      <div className='bg-opacity-30  inset-0 backdrop-blur-sm flex justify-center items-center fixed z-10'>
       <div className='flex w-[90%] sm:w-auto flex-col gap-1  text-white'>
         <button onClick={change} className=' rounded-full place-self-end'><MdCancel size={35} /></button>
-        <div className='bg-tertiary rounded-xl py-6 px-4 md:py-8 md:px-6 text-center'>
+        <div className='bg-primary rounded-xl py-8 px-6 md:py-10 md:px-8 text-center'>
           <h1 className='text-2xl md:text-3xl font-semibold outline outline-1 outline-accent'>Click here to Subscribe!</h1>
           <p className=' my-4 text-lg md:text-xl  text-accent'>Let's connect for more project and work</p>
           <form name='contact-us' className='space-y-5 w-full max-w-[780px]' method='POST'  data-netlify="true" >

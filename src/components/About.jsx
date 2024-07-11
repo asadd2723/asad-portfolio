6f2f19b187aefd21e2b124a23c7e2ed5c3019a1e
asadd2723 <asadu3040@gmail.com>
2024-07-11 14:06:41 +0500

    about page navigation is not working

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from '../assets/img/about.webp'
 function About() {
   return (
-    <section className='section bg-secondary'>
+    <section id='about' className='section bg-secondary'>
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-24">
           <img className='object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl' src={Image} alt="" />

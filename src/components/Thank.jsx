b0f946172f6a7fcaa89570bce8d36fd9d37f0a69
asadd2723 <asadu3040@gmail.com>
2024-07-14 00:06:00 +0500

    changing onclick funtion in thank page

--- src/components/Thank.jsx
+++ src/components/Thank.jsx
@@ -7,6 +7,10 @@ import { resetFormState } from '../store/formSlice'
 function Thank() {
   const disptach = useDispatch()
   const { isSubmitting, isSubmitted, error } = useSelector((state) => state.form);
+
+  const changed = ()=>{
+    disptach(resetFormState())
+  }
   return (
     isSubmitted && <div className='bg-opacity-30  inset-0 backdrop-blur-sm flex justify-center items-center fixed z-20 '>
       <motion.div initial={{opacity:0}} 
@@ -17,7 +21,7 @@ function Thank() {
           ease:"easeOut"
         }}} 
         className='flex sm:w-auto flex-col gap-1  text-white'>
-        <button onClick={()=>disptach(resetFormState())} className=' rounded-full place-self-end'><MdCancel size={35} /></button>
+        <button onClick={changed} className=' rounded-full place-self-end'><MdCancel size={35} /></button>
         <div className='bg-primary rounded-xl w-[324px] h-[320px] md:w-[390px] px-6 md:py-10 md:px-8 text-center flex flex-col justify-center gap-3 items-center'>
           <h1 className='text-3xl md:text-4xl font-semibold w-full outline outline-1 outline-accent'>Thank you!</h1>
           <span className='my-4 text-md md:text-xl text-accent'>We appreciate your feedback. We'll get back to you shortly.</span>
